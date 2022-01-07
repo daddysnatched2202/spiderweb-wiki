165ce9142d21827a55488dcc0502befa7a4387ce
Curtis Klasen <daddysnatched2202@gmail.com>
2022-01-07 11:48:59 -0500

    Minor

--- util.lisp
+++ util.lisp
@@ -16,7 +16,7 @@
 
 (in-package :web)
 
-(defparameter *base-path* "/home/curtis/Documents/programming/lisp/web2")
+(defparameter *base-path* "")
 
 (defun matching-symbols (test-fun tree)
   (let ((l))
