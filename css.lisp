efe0a1b1d59d327ec743886c77729fee51b850f4
Curtis Klasen <daddysnatched2202@gmail.com>
2022-01-28 10:12:24 -0500

    Refactor css.lisp

--- css.lisp
+++ css.lisp
@@ -51,27 +51,31 @@
   `(css/with-nord-palette
      (list ,@(mapcar #λ(cond ((nord-sym? _0)
 			      _0)
+			     ((null _0)
+			      nil)
+			     ((listp _0)
+			      (list 'css/nord-list _0))
 			     ((symbolp _0)
 			      (list 'quote _0))
 			     (t _0))
 		     els))))
 
 (defun css/std ()
-  (let ((rules (list (css/nord-list footer :position absolute
-					   :bottom 0px
-					   :left 0px
-					   :height 3em
-					   :width 100%
-					   :background-color nord1
-				    (css/nord-list a :bottom -15px
-						     :margin 5px
-						     :position relative))
-		     (css/nord-list body :background-color nord0
-					 :color nord5)
-		     (css/nord-list p :color nord5)
-		     (css/nord-list a :background-color nord2
-				      :color nord5
-				      :text-decoration none
-				      :padding 4px)
-		     (css/nord-list "a:hover" :background-color nord3))))
+  (let ((rules (css/nord-list footer :position absolute
+				     :bottom 0px
+				     :left 0px
+				     :height 3em
+				     :width 100%
+				     :background-color nord1
+			      (a :bottom -15px
+				 :margin 5px
+				 :position relative)
+			      (body :background-color nord0
+				    :color nord5)
+			      (p :color nord5)
+			      (a :background-color nord2
+				 :color nord5
+				 :text-decoration none
+				 :padding 4px)
+			      ("a:hover" :background-color nord3))))
     (lass:compile-and-write rules)))
