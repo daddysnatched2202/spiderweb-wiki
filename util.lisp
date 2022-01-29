7f9d81b1674536de4d46a1434093753b646f0252
Curtis Klasen <daddysnatched2202@gmail.com>
2022-01-29 08:53:16 -0500

    Fix λ-macro documentation

--- util.lisp
+++ util.lisp
@@ -62,7 +62,7 @@
 ;;; Macro to speed up creation of lambdas
 ;;; Automatically binds symbols in the body of the form (_n : n ∈ ℤ) to the
 ;;; nth argument of the lambda
-;;; Example : (λ-macro (+ _1 _2)) -> (lambda (_1 _2) (+ 1 2))
+;;; Example : (λ-macro (+ _1 _2)) -> (lambda (_1 _2) (+ _1 _2))
 ;;; Even handles discontinuous argument lists !
 (defmacro λ-macro (&body body)
   (let* ((bound-in-body (am:->> body
