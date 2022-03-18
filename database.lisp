156f2610b505dae3a16a2ef73cb1782a1df50dbe
Curtis Klasen <daddysnatched2202@gmail.com>
2022-03-18 14:34:04 -0400

    Use a metaclass for classes that are both persistent and indexed

--- database.lisp
+++ database.lisp
@@ -16,11 +16,10 @@
 
 (in-package :web)
 
-(defvar *nodes* nil)
-(defvar *notes* nil)
-(defvar *links* nil)
+(defclass persistent-indexed-class (b.d:persistent-class b.i:indexed-class)
+  ())
 
-(defclass note (serializable)
+(defclass note ()
   ((content
     :initarg :content
     :reader note/content)
@@ -29,44 +28,50 @@
     :reader note/type)
    (path
     :initarg :path
-    :reader note/path)
-   (class-spec
-    :initform (let ((n (find-class 'note)))
-		(make-instance 'class-spec
-			       :ref n
-			       :slot-specs (list
-					    (make-slot-spec n
-							    'content
-							    nil)
-					    (make-slot-spec n
-							    'type
-							    nil)
-					    (make-slot-spec n
-							    'path
-							    nil)))))))
+    :reader note/path
+    :index-type b.i:hash-list-index
+    :index-reader note/all-with-node))
+  (:metaclass persistent-indexed-class))
 
-(defclass node (serializable)
+(defclass node ()
   ((name
     :initarg :name
-    :reader node/name)
-   (class-spec
-    :initform (let ((n (find-class 'node)))
-		(make-instance 'class-spec
-			       :ref n
-			       :slot-specs (list (make-slot-spec n
-								 'name
-								 nil)))))))
+    :reader node/name
+    :index-type b.i:string-unique-index
+    :index-reader node/with-name))
+  (:metaclass persistent-indexed-class))
+
+(defclass breakout-node ()
+  ((parent
+    :initarg :parent
+    :reader breakout-node/parent)
+   (breakout
+    :initarg :breakout
+    :reader breakout-node/breakout))
+  (:metaclass persistent-indexed-class))
 
 (defclass link ()
   ((text
     :initarg :text
-    :reader link/text)
+    :accessor link/text)
    (from
     :initarg :from
-    :reader link/from)
+    :accessor link/from)
    (to
     :initarg :to
-    :reader link/to)))
+    :accessor link/to))
+  (:metaclass b.d:persistent-class))
+
+(defun note/all ()
+  (b.d:store-objects-with-class 'note))
+
+(defun link/all ()
+  (b.d:store-objects-with-class 'link))
+
+(defun link/exists? (from to)
+  (find-if #λ(and (path= from (link/from _0))
+		  (path= to (link/to _0)))
+	   (link/all)))
 
 (defun path= (a b)
   (string= (path->string a)
@@ -81,9 +86,6 @@
 (defun note/has-node? (note node)
   (member node (note/path note)))
 
-(defun note/all-with-node (node)
-  (remove-if-not #λ(note/has-node? _0 node) *notes*))
-
 (defun note/with-path (path)
   (loop for n in (note/all-with-node (car path))
 	if (path= (note/path n) path)
@@ -97,9 +99,6 @@
 		 notes)))
     (rec (cdr path) (note/all-with-node (car path)))))
 
-(defun node/find-with-name (name)
-  (find name *nodes* :key #'node/name :test #'string=))
-
 (let ((space '(" " . "-"))
       (break-char #\&)
       (sep #\:))
@@ -108,17 +107,16 @@
 			(str:replace-all (car space) (cdr space))
 			(str:downcase)))
 	   (split-conv (str:split break-char rem-space)))
-      (ana:aif (node/find-with-name rem-space)
-	  ana:it
-	  (let ((node (if (> (length split-conv) 1)
-			  (make-instance 'breakout-node
-					 :name rem-space
-					 :breakout (cdr split-conv)
-					 :parent (car split-conv))
-			  (make-instance 'node
-					 :name rem-space))))
-	    (push node *nodes*)
-	    node))))
+      (ana:aif (node/with-name rem-space)
+	       ana:it
+	       (b.d:with-transaction ()
+		   (if (> (length split-conv) 1)
+		       (make-instance 'breakout-node
+				      :name rem-space
+				      :breakout (cdr split-conv)
+				      :parent (car split-conv))
+		       (make-instance 'node
+				      :name rem-space))))))
 
   (defun string->path (str)
     (am:-<>> (str:split sep str :omit-nulls t)
@@ -139,16 +137,21 @@
     links))
 
 (defun link/new (from to text)
-  (let* ((l (make-instance 'link
-			   :from from
-			   :to to
-			   :text text)))
-    (when (find l *links* :test #'link=)
-      (link/delete l))
-    (push l *links*)))
+  (labels ((maker ()
+	     (ana:aif (link/exists? from to)
+		      (setf (link/from ana:it) from
+			    (link/to ana:it) to
+			    (link/text ana:it) text)
+		      (make-instance 'link
+				     :from from
+				     :to to
+				     :text text))))
+    (b.d:with-transaction ()
+      (maker))))
 
 (defun link/delete (l)
-  (setq *links* (delete l *links* :test #'link=)))
+  (b.d:with-transaction ()
+    (b.d:delete-object l)))
 
 (defun note/new (path-str content &key (type :text/markdown))
   (let ((p (string->path path-str)))
@@ -158,50 +161,55 @@
 				:path p
 				:type type
 				:content content)))
-	  (push n *notes*)
-	  (mapcar #λ(link/new (car _0)
+	  (b.d:with-transaction ()
+	    (mapcar #λ(link/new (car _0)
 			      (cadr _0)
 			      (ana:aif (caddr _0)
 				       ana:it
 				       (car _0)))
-		  (find-links content))
+		    (find-links content)))
 	  n))))
 
 (defun note/delete (path)
-  (setf *notes* (remove-if #λ(path= (note/path _0) path)
-			   *notes*)))
+  (b.d:with-transaction ()
+    ;; delete links
+    (b.d:delete-object (note/with-path path))))
 
 (defun note/move (old-path new-path)
   (let* ((n (note/with-path old-path))
 	 (cont (note/content n))
 	 (type (note/type n)))
-    (note/delete old-path)
-    (note/new (path->string new-path)
-	      cont
-	      :type type)))
+    (b.d:with-transaction ()
+      (note/delete old-path)
+      (note/new (path->string new-path)
+		cont
+		:type type))))
 
 (defun db/clear ()
-  (setf *notes* nil))
-
-(defun file-contents (path)
-  (with-open-file (stream path :direction :input)
-    (let ((contents (make-string (file-length stream))))
-      (read-sequence contents stream)
-      contents)))
-
-(defun db/load-local (path)
-  (am:-> path
-    (file-contents)
-    (jonathan:parse)))
-
-(defun db/save-local (path)
-  (with-open-file (stream path :direction :output :if-exists :supersede)
-    (am:->> *notes*
-      (general->serial)
-      (jonathan:to-json)
-      (format stream "~a"))))
+  (b.d:with-transaction ()
+    (loop for obj in (b.d:all-store-objects)
+	  do (b.d:delete-object obj))))
 
 (defun db/load-credentials ()
   (if (eq *storage-type* :s3)
       (setf zs3:*credentials* (zs3:file-credentials (make-rel-path "s3")))
       (error "Trying to load s3 credentials when *storage-type* is not set for s3")))
+
+(defun db/load-local (path)
+  (make-instance 'b.d:mp-store
+		 :directory path
+		 :subsystems (list (make-instance 'b.d:store-object-subsystem))))
+
+(defun db/store-objects-of-classes (&rest classes)
+  (am:->> classes
+    (mapcar #'b.d:store-objects-with-class)
+    (apply #'append)))
+
+(defun db/close ()
+  (b.d:close-store))
+
+(defun db/all-notes ()
+  (b.d:store-objects-with-class 'note))
+
+(defun db/all-nodes ()
+  (db/store-objects-of-classes 'node 'breakout-node))
