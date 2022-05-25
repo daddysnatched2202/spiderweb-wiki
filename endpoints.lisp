b8542ffa5737009e53a303c234b0e925cf96460a
Curtis Klassen <daddysnatched2202@gmail.com>
2022-05-25 15:32:12 -0400

    Fix `ningle/cache-file` documentation

--- endpoints.lisp
+++ endpoints.lisp
@@ -18,8 +18,7 @@
 
 (defvar *app* (make-instance 'ningle:app))
 
-;;; returns a function that can be used in an end point for caching static files on
-;;; the client
+;;; can be used in an end point for caching static files on the client
 ;;; file-type should be a string which will be passed to ningle/respond-type, or nil
 (defun ningle/cache-file (file hash &key (file-type nil))
   (let* ((req (lack.request:request-headers ningle:*request*))
