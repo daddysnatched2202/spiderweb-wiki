1363f96d9d476f7a5bc18583a8f6ce7fc89792ce
Curtis Klassen <daddysnatched2202@gmail.com>
2022-06-21 09:59:39 -0400

    Fix `ningle/set-response-status`
    Need to use `princ-to-string` instead of `string`

--- util.lisp
+++ util.lisp
@@ -112,7 +112,7 @@ params `~a`, got params `~a`~%"
 (defun ningle/set-response-status (code)
   (setf (lack.response:response-status ningle:*response*)
 	code)
-  (string code))
+  (princ-to-string code))
 
 ;;; can be used in an end point for caching static files on the client
 ;;; file-type should be a string which will be passed to ningle/respond-type, or nil
