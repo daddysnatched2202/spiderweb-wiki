0d5e3d5e6d5722ac605e09adf1c61e1c553ab42e
Curtis Klassen <daddysnatched2202@gmail.com>
2022-08-10 15:05:32 -0400

    Improve `3bmd-wiki:process-wiki-link`
    Replace the `gen-format-string` function with `generate-link`
    
    Instead of generating a format string and running a second `format`, we only need to
    do it once

--- html.lisp
+++ html.lisp
@@ -77,29 +77,24 @@
   (let ((link-text (ana:aif (car args)
                             ana:it
                             (am:-> formatted-target
-                                   (string->path)
-                                   (path->string)))))
-    (labels ((gen-format-string (target label)
-               (format nil
+                              (string->path)
+                              (path->string)))))
+    (labels ((generate-link (target label)
+               (format stream
                        "<a href=\"~a\">~a</a>"
                        target
                        label)))
-            (cond ((and (cadr args)
-                        (string= (cadr args) "url"))
-                   (format stream
-                           (gen-format-string "~a" "~a")
-                           formatted-target
-                           link-text))
-                  ((or (and (cadr args)
-                            (string= (cadr args) "note"))
-                       (null (cadr args)))
-                   (format stream
-                           (gen-format-string "~a" "~a")
-                           (path->url formatted-target)
-                           link-text))
-                  (t (format stream
-                             (gen-format-string "/wiki" "BAD LINK: ~a")
-                             formatted-target))))))
+      (cond ((and (cadr args)
+                  (string= (cadr args) "url"))
+             (generate-link formatted-target
+                            link-text))
+            ((or (and (cadr args)
+                      (string= (cadr args) "note"))
+                 (null (cadr args)))
+             (generate-link (path->url formatted-target)
+                            link-text))
+            (t (generate-link "/wiki"
+                              (format nil "BAD LINK ~a" formatted-target)))))))
 
 (setf 3bmd-wiki:*wiki-links* t)
 (setf 3bmd-wiki:*wiki-processor* (make-instance 'wiki-parser))
